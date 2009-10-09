2d79881486f752c0c8596082ce9c1d3f652b8251
Christopher John CZETTEL <christopher@czettel.com>
2009-10-09 16:01:04 +0200

    Modified views and successurl redirect

--- app/controllers/dao_pay_controller.rb
+++ app/controllers/dao_pay_controller.rb
@@ -1,20 +1,21 @@
 class DaoPayController < ApplicationController
   
   def index
+  end
+  
+  def new
     @myParams = DaoPayIframeParameter.new
-    @myParams.price = 20.22
-    @myParams.orderno = 10016969
-
+    @myParams.price = rand(10) + 1
+    @myParams.successurl = "http://localhost:3000/DaoPay/show"
+    #@myParams.orderno = "10026969" + (rand(9)+1).to_s
+    
     respond_to do |format|
       format.html
       format.xml  { render :xml => 'OK' }
     end
   end
   
-  def new
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => 'OK' }
-    end
+  def show
+    @response = params
   end
 end
--- app/helpers/dao_pay_helper.rb
+++ app/helpers/dao_pay_helper.rb
@@ -1,8 +1,8 @@
 module DaoPayHelper
   
   def daopay_iframe(param)
-    "<iframe height=\"550\" width=\"400\" marginheight=\"0\" marginwidth=\"0\" 
-    frameborder=\"0\" src=\"#{param.payment_url}\"> 
+    "<iframe height=\"550\" width=\"415\" marginheight=\"0\" marginwidth=\"0\" 
+    frameborder=\"0\" src=\" #{param.payment_url}\"> 
     </iframe>"
   end
 end
