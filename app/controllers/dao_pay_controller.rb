44c2e69afae11cbc3108141e3117fd4dc3b28e01
Christopher John CZETTEL <christopher@czettel.com>
2009-10-09 15:17:51 +0200

    added payment_url method to model, and helper for iframe

--- app/controllers/dao_pay_controller.rb
+++ app/controllers/dao_pay_controller.rb
@@ -2,13 +2,9 @@ class DaoPayController < ApplicationController
   
   def index
     @myParams = DaoPayIframeParameter.new
-    
-    @myParams.appcode = DAOPAY_APP_CONFIG['appcode']
     @myParams.price = 20.22
     @myParams.orderno = 10016969
-    
-    @myParams.save!
-    
+
     respond_to do |format|
       format.html
       format.xml  { render :xml => 'OK' }
--- app/models/dao_pay_iframe_parameter.rb
+++ app/models/dao_pay_iframe_parameter.rb
@@ -1,4 +1,7 @@
 class DaoPayIframeParameter < ActiveRecord::BaseWithoutTable
+  require 'cgi'
+  
+  
   column :appcode,      :integer  #5 digit code of website 
   column :price,        :float    #price of respective product
   column :currency,     :string   #3 digit currency
@@ -8,8 +11,9 @@ class DaoPayIframeParameter < ActiveRecord::BaseWithoutTable
   column :affiliate,    :string   #affiliate code, used to assign payment to affiliate
   column :style,        :string   #code for choosing style of DaoPay mask
   column :mcx,          :string   #specifies if upselling should be enabled
-  column :orderno,      :string  #internal order number to be displayed
-  #column :user_defined          #any other user defined attributes that will be passed through DaoPay
+  column :orderno,      :string   #internal order number to be displayed
+  column :gateway_url,  :string   #url of DaoPay gateway
+  #column :user_defined           #any other user defined attributes that will be passed through DaoPay
   
   
   validates_presence_of :appcode
@@ -28,4 +32,22 @@ class DaoPayIframeParameter < ActiveRecord::BaseWithoutTable
                          :in => %w( call sms both ), 
                          :message => "method {{value}} is not supported (try call/sms/both)",
                          :allow_nil => true
+  
+  def initialize
+    super
+    self.attributes = DAOPAY_APP_CONFIG
+  end
+  
+  def payment_url
+    self.save!
+    elements = []
+
+    self.attributes.each do |key, value|
+      unless (value.nil? || key == 'gateway_url')
+            elements << "#{CGI.escape(key)}=#{CGI.escape(value.to_s)}"
+      end
+    end
+
+    self.gateway_url + "?" +  elements.join('&')
+  end
 end
